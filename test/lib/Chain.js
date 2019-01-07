eb93d49ac5fb5ad8512394770e28509cd4ab7ecc
albertovilva <albert.vilva@gmail.com>
2019-01-07 16:56:46 +0100

    Fixed yet another bug with cases in file names

--- test/lib/Chain.js
+++ test/lib/Chain.js
@@ -1,7 +1,7 @@
 const chai = require("chai")
 const expect = chai.expect
 const Chain = require("../../src/lib/Chain")
-const NGramGenerator = require("../../src/lib/NGramGenerator")
+const NgramGenerator = require("../../src/lib/NgramGenerator")
 const {splitIntoWords, removeUnwantedBlocks} = require('../../src/lib/processors/words');
 
 const trainingText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -12,7 +12,7 @@ const options = {
   order: 3,
 }
 
-const ngramWeights = new NGramGenerator(trainingText, options).getNgrams();
+const ngramWeights = new NgramGenerator(trainingText, options).getNgrams();
 
 describe("Chain", () => {
 
